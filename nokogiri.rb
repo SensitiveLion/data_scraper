be87da7173c32650bb012d15d4f0b794264b3553
SensitiveLion <andypassidomo@gmail.com>
2015-05-23 14:29:15 -0400

    parsed through wikipedia data for Learnit.ninja

--- nokogiri.rb
+++ nokogiri.rb
@@ -2,12 +2,27 @@ require "nokogiri"
 require "open-uri"
 require "pry"
 
-doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Tre_Fontane_Abbey"))
+doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Apple_Inc."))
+header = []
+sub_header = []
 text = []
+
+doc.css('p + h2').each do |h|
+  if h.text != "Contents" && h.text != "References"&& h.text != "Further reading" && h.text != "External links"
+    header << h.text
+  end
+end
+
+doc.css('p + h3').each do |s_h|
+  sub_header << s_h.text
+end
+
 doc.css('p').each do |p|
   if p.text.length != 0
     text << p.text
   end
 end
+
+
 
 binding.pry
